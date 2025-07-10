7037bb58775e5911ab7b7c64737ace58da17e6d2
Meigo™ Corporation <73817505+meigoc@users.noreply.github.com>
2025-07-11 00:43:58 +0300

    BMModelCommand: Updated BetterModel API from 1.5.5 to 1.8.1
    Now use EntityTrackerRegistry

--- src/main/java/net/openproject/dbettermodel/commands/BMModelCommand.java
+++ src/main/java/net/openproject/dbettermodel/commands/BMModelCommand.java
@@ -8,10 +8,8 @@ import com.denizenscript.denizencore.scripts.commands.generator.ArgName;
 import com.denizenscript.denizencore.scripts.commands.generator.ArgPrefixed;
 import com.denizenscript.denizencore.utilities.debugging.Debug;
 import kr.toxicity.model.api.BetterModel;
-import kr.toxicity.model.api.data.renderer.RenderInstance;
-import kr.toxicity.model.api.tracker.EntityTracker;
+import kr.toxicity.model.api.tracker.EntityTrackerRegistry;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.LivingEntity;
 
 public class BMModelCommand extends AbstractCommand {
 
@@ -37,49 +35,43 @@ public class BMModelCommand extends AbstractCommand {
     // @Usage
     // Use to add a model to an entity.
     // - bmmodel entity:<context.entity> model:my_model
+    //
+    // Use to remove a model from an entity.
+    // - bmmodel entity:<context.entity> model:my_model remove
     // -->
 
     @Override
     public void addCustomTabCompletions(TabCompletionsBuilder tab) {
-        tab.addWithPrefix("model:", BetterModel.inst().modelManager().keys());
+        tab.addWithPrefix("model:", BetterModel.plugin().modelManager().keys());
     }
 
     public static void autoExecute(ScriptEntry scriptEntry,
                                    @ArgName("entity") @ArgPrefixed EntityTag entityTag,
                                    @ArgName("model") @ArgPrefixed ElementTag model,
-                                   @ArgName("remove") boolean remove) throws Exception {
+                                   @ArgName("remove") boolean remove) {
 
-        Entity entity = entityTag.entity;
-        if (!(entity instanceof LivingEntity)) {
-            Debug.echoError("Entity must be a living entity");
-            return;
-        }
+        Entity entity = entityTag.getBukkitEntity();
         if (model == null) {
             Debug.echoError("Model is not specified");
             return;
         }
+        String modelName = model.asString();
+        
         if (remove) {
-            EntityTracker tr = EntityTracker.tracker(entity);
-            if (tr != null) tr.close();
+            EntityTrackerRegistry registry = EntityTrackerRegistry.registry(entity.getUniqueId());
+            if (registry != null) {
+                if (registry.remove(modelName)) {
+                    Debug.echoApproval("Model '" + modelName + "' removed from entity.");
+                } else {
+                    Debug.echoError("Model '" + modelName + "' not found on entity.");
+                }
+            } else {
+                Debug.echoError("Entity does not have any models.");
+            }
             return;
         }
 
-        BetterModel.inst()
-                .modelManager()
-                .renderer(model.toString())
-                .create(entity);
-
-        EntityTracker tracker = EntityTracker.tracker(entity);
-        if (tracker == null) {
-            Debug.echoError("Failed to create model tracker");
-            return;
-        }
-        RenderInstance inst = tracker.getInstance();
-        if (inst == null) {
-            Debug.echoError("Failed to get model instance");
-            return;
-        }
-
-        tracker.spawnNearby(entity.getLocation());
+        BetterModel.plugin().modelManager().renderer(modelName).create(entity);
+        Debug.echoApproval("Model '" + modelName + "' added to entity.");
     }
 }
