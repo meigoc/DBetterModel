7ce0739eacab00ee187d6a1a5624ad6a31c2a2d3
Meigo™ Corporation <73817505+meigoc@users.noreply.github.com>
2025-07-24 22:26:29 +0300

    Add multiple models support + Upgraded to BetterModelAPI 1.10.1

--- src/main/java/net/openproject/dbettermodel/commands/BMModelCommand.java
+++ src/main/java/net/openproject/dbettermodel/commands/BMModelCommand.java
@@ -8,41 +8,44 @@ import com.denizenscript.denizencore.scripts.commands.generator.ArgName;
 import com.denizenscript.denizencore.scripts.commands.generator.ArgPrefixed;
 import com.denizenscript.denizencore.utilities.debugging.Debug;
 import kr.toxicity.model.api.BetterModel;
-import kr.toxicity.model.api.tracker.EntityTrackerRegistry;
 import org.bukkit.entity.Entity;
+
+import java.util.Optional;
 
 public class BMModelCommand extends AbstractCommand {
 
     public BMModelCommand() {
         setName("bmmodel");
-        setSyntax("bmmodel [entity:<entity>] [model:<model>] (remove)");
+        setSyntax("bmmodel entity:<entity> model:<model> (remove)");
         autoCompile();
     }
 
     // <--[command]
     // @Name BMModel
-    // @Syntax bmmodel [entity:<entity>] [model:<model>] (remove)
+    // @Syntax bmmodel entity:<entity> model:<model> (remove)
     // @Required 2
-    // @Short Adds or removes a model from an entity
+    // @Short Adds or removes a model from an entity.
     // @Group DBetterModel
     //
     // @Description
-    // Adds or removes a model from an entity.
+    // Adds or removes a specific model from an entity. This is necessary for entities that can have multiple models.
     //
     // @Tags
     // <EntityTag.bm_entity>
+    // <BMEntityTag.model[<name>]>
     //
     // @Usage
     // Use to add a model to an entity.
     // - bmmodel entity:<context.entity> model:my_model
     //
+    // @Usage
     // Use to remove a model from an entity.
     // - bmmodel entity:<context.entity> model:my_model remove
     // -->
 
     @Override
     public void addCustomTabCompletions(TabCompletionsBuilder tab) {
-        tab.addWithPrefix("model:", BetterModel.plugin().modelManager().keys());
+        tab.addWithPrefix("model:", BetterModel.models().stream().map(m -> m.name()).toList());
     }
 
     public static void autoExecute(ScriptEntry scriptEntry,
@@ -52,26 +55,30 @@ public class BMModelCommand extends AbstractCommand {
 
         Entity entity = entityTag.getBukkitEntity();
         if (model == null) {
-            Debug.echoError("Model is not specified");
+            Debug.echoError("Model is not specified.");
             return;
         }
         String modelName = model.asString();
-        
+
         if (remove) {
-            EntityTrackerRegistry registry = EntityTrackerRegistry.registry(entity.getUniqueId());
-            if (registry != null) {
+            BetterModel.registry(entity).ifPresentOrElse(registry -> {
                 if (registry.remove(modelName)) {
                     Debug.echoApproval("Model '" + modelName + "' removed from entity.");
                 } else {
                     Debug.echoError("Model '" + modelName + "' not found on entity.");
                 }
-            } else {
+            }, () -> {
                 Debug.echoError("Entity does not have any models.");
-            }
+            });
             return;
         }
 
-        BetterModel.plugin().modelManager().renderer(modelName).create(entity);
-        Debug.echoApproval("Model '" + modelName + "' added to entity.");
+        Optional.ofNullable(BetterModel.plugin().modelManager().renderer(modelName)).ifPresentOrElse(
+                renderer -> {
+                    renderer.create(entity);
+                    Debug.echoApproval("Model '" + modelName + "' added to entity.");
+                },
+                () -> Debug.echoError("Model renderer '" + modelName + "' not found.")
+        );
     }
 }
